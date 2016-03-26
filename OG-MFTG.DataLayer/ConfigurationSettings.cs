3cbba2b581dfb400e337859211defe4199005562
Jonathan Guinto <xianlee.ivan.guinto@gmail.com>
2016-03-26 16:23:59 +0800

    change c# version to 6.0

---
+++ OG-MFTG.DataLayer/ConfigurationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+
+namespace OG_MFTG.DataLayer
+{
+    public class ConfigurationSettings
+    {
+        private static string _connectionString;
+
+        public static string GetConnectionString()
+        {
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            }
+            return _connectionString;
+        }
+    }
+}
