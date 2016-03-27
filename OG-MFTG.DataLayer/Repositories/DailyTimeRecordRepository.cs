9b3ce612e0d324ab8cf5230ed621e7b6281a9d5e
Jonathan Guinto <xianlee.ivan.guinto@gmail.com>
2016-03-27 12:00:44 +0800

    add repository for dailytimerecord and timeCategory

---
+++ OG-MFTG.DataLayer/Repositories/DailyTimeRecordRepository.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using HR_Department.Models.Tables;
+
+namespace OG_MFTG.DataLayer.Repositories
+{
+    public class DailyTimeRecordRepository : IIO<DailyTimeRecord>
+    {
+        public async Task<IEnumerable<DailyTimeRecord>> SelectAll()
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+
+                return
+                    await
+                        connection.QueryAsync<DailyTimeRecord>("DailyTimeRecordAll",
+                            commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<DailyTimeRecord>> SelectById(int id)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@DailyTimeRecordId", id);
+                return
+                    await
+                        connection.QueryAsync<DailyTimeRecord>("DailyTimeRecordSelectById", p,
+                            commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+
+        public async Task Insert(DailyTimeRecord model)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@EmployeeId", model.EmployeeId);
+                p.Add("@TimeCategoryId", model.TimeCategoryId);
+                p.Add("@DateCreated", model.DateCreated);
+                p.Add("@Time", model.Time);
+                await connection.ExecuteAsync("DailyTimeRecordInsert", p, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@DailyTimeRecordId", id);
+                await connection.ExecuteAsync("DailyTimeRecordDelete", p, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+
+        public async Task Update(DailyTimeRecord model)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@DailyTimeRecordId", model.DailyTimeRecordId);
+                p.Add("@EmployeeId", model.EmployeeId);
+                p.Add("@TimeCategoryId", model.TimeCategoryId);
+                p.Add("@DateCreated", model.DateCreated);
+                p.Add("@Time", model.Time);
+                await connection.ExecuteAsync("DailyTimeRecordUpdate", p, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<DailyTimeRecord>> SelectByEmployeeId(int employeeId)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@EmployeeId", employeeId);
+                return
+                    await
+                        connection.QueryAsync<DailyTimeRecord>("DailyTimeRecordSelectByEmployeeId", p,
+                            commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        } 
+    }
+}
---
+++ OG-MFTG.DataLayer/Repositories/TimeCategoryRepository.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using HR_Department.Models.Tables;
+
+namespace OG_MFTG.DataLayer.Repositories
+{
+    public class TimeCategoryRepository : IIO<TimeCategory>
+    {
+        public async Task<IEnumerable<TimeCategory>> SelectAll()
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+
+                return
+                    await
+                        connection.QueryAsync<TimeCategory>("TimeCategorySelectAll",
+                            commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<TimeCategory>> SelectById(int id)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@TimeCategoryId", id);
+                return
+                    await
+                        connection.QueryAsync<TimeCategory>("TimeCategorySelectById", p,
+                            commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task Insert(TimeCategory model)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@TimeCategoryValue", model.TimeCategoryValue);
+                await connection.ExecuteAsync("TimeCategoryInsert", p, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@TimeCategoryId", id);
+                await connection.ExecuteAsync("TimeCategoryDelete", p, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task Update(TimeCategory model)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@TimeCategoryValue", model.TimeCategoryValue);
+                p.Add("@TimeCategoryId", model.TimeCategoryId);
+                await connection.ExecuteAsync("TimeCategoryUpdate", p, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<TimeCategory>> SelectByValue(string value)
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationSettings.GetConnectionString());
+                var p = new DynamicParameters();
+
+                p.Add("@Value", value);
+                return await
+                    connection.QueryAsync<TimeCategory>("TimeCategorySelectByValue", p,
+                        commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
